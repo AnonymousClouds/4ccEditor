008766d624c32b72d856863ac18576b0c142cd13
AnonymousClouds <AnonymousClouds@proton.me>
2026-09-19 08:35:52 -0500

    Update version number

--- main.cpp
+++ main.cpp
@@ -204,7 +204,7 @@ int APIENTRY _tWinMain(HINSTANCE I, HINSTANCE PI, LPTSTR CL, int SC)
 	ghw_main = CreateWindowEx(
 		0,
 		wc.lpszClassName,
-		_T("4ccEditor Autumn 26 Friendlies Edition (Version C)"),
+		_T("4ccEditor Autumn 26 Friendlies Edition (Version D)"),
 		WS_OVERLAPPEDWINDOW,
 		20, 20, 1320+144, 700,
 		NULL, NULL, ghinst, NULL);
